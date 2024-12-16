129f766501b70f58d219a1dcff220d63b6a5ec82
Duplication1 <gamot.kim.fernandez@gmail.com>
2024-12-16 15:06:31 +0800

    landing page design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,9 @@
+
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="bg-green-300">
         @csrf
 
         <!-- Email Address -->
---
+++ resources/views/store.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome to the New Page</h1>
+        <p>This is your new page content.</p>
+    </div>
+@endsection
